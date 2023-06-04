75ae45ff015540be50d7a7bede4eed7d2a00a3fe
Michael Rivera <soymrmike@gmail.com>
2023-06-04 01:44:05 -0700

    Working with postgresql.

--- complete/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ complete/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AccessingDataMysqlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccessingDataMysqlApplication.class, args);
+      SpringApplication.run(AccessingDataMysqlApplication.class, args);
 	}
 }
--- complete/src/main/java/com/example/accessingdatamysql/User.java
+++ complete/src/main/java/com/example/accessingdatamysql/User.java
@@ -1,11 +1,13 @@
 package com.example.accessingdatamysql;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class
+@Table(name="users")
 public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
