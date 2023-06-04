6e255471c190225fe2e6d5b312c451ee8f1f6eb7
Michael Rivera <soymrmike@gmail.com>
2023-06-04 03:23:17 -0700

    Adjusted indentation.

--- complete/src/main/java/com/example/accessingdatamysql/AccessingDataPgApplication.java
+++ complete/src/main/java/com/example/accessingdatamysql/AccessingDataPgApplication.java
@@ -5,21 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
+    
 // @SpringBootApplication
 @ComponentScan
 public class AccessingDataPgApplication {
+    
+    public static void main(String[] args) {
+        // SpringApplication.run(AccessingDataPgApplication.class, args);
 
-	public static void main(String[] args) {
-      // SpringApplication.run(AccessingDataPgApplication.class, args);
-
-      AnnotationConfigApplicationContext appCtx = new AnnotationConfigApplicationContext(AccessingDataPgApplication.class);
-      for (String beanName : appCtx.getBeanDefinitionNames()) {
-          System.out.println(beanName);
-      }
-      System.out.println("Done printing beans");
-
-      MyTool t = appCtx.getBean(MyTool.class);
-      t.run();
-            
-	}
+        AnnotationConfigApplicationContext appCtx = new AnnotationConfigApplicationContext(AccessingDataPgApplication.class);
+        for (String beanName : appCtx.getBeanDefinitionNames()) {
+            System.out.println(beanName);
+        }
+        System.out.println("Done printing beans");
+        
+        MyTool t = appCtx.getBean(MyTool.class);
+        t.run();
+    }
 }
