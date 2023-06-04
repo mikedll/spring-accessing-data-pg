e66f86ebc9302d551da6f6f6e3208409f5453a07
Michael Rivera <soymrmike@gmail.com>
2023-06-04 02:31:21 -0700

    Got an autowire example to work.

--- complete/src/main/java/com/example/accessingdatamysql/AccessingDataPgApplication.java
+++ complete/src/main/java/com/example/accessingdatamysql/AccessingDataPgApplication.java
@@ -2,11 +2,23 @@ package com.example.accessingdatamysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+// @SpringBootApplication
+@ComponentScan
 public class AccessingDataPgApplication {
 
 	public static void main(String[] args) {
-      SpringApplication.run(AccessingDataPgApplication.class, args);
+      // SpringApplication.run(AccessingDataPgApplication.class, args);
+
+      AnnotationConfigApplicationContext appCtx = new AnnotationConfigApplicationContext(AccessingDataPgApplication.class);
+      for (String beanName : appCtx.getBeanDefinitionNames()) {
+          System.out.println(beanName);
+      }
+      System.out.println("Done printing beans");
+
+      MyTool t = appCtx.getBean(MyTool.class);
+      t.run();
 	}
 }
---
+++ complete/src/main/java/com/example/accessingdatamysql/OtherClass.java
@@ -0,0 +1,12 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OtherClass {
+
+    public String hello() {
+        return "Hello Mike";
+    }
+    
+}
