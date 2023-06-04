c714f9625901894282c5cd85c4eaa398354ec72f
Michael Rivera <soymrmike@gmail.com>
2023-06-04 05:51:10 -0700

    Providing data source ourselves.

--- complete/src/main/java/com/example/accessingdatamysql/AccessingDataPgApplication.java
+++ complete/src/main/java/com/example/accessingdatamysql/AccessingDataPgApplication.java
@@ -1,20 +1,32 @@
 package com.example.accessingdatamysql;
 
+import javax.sql.DataSource;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
+import org.springframework.util.ClassUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.jdbc.DatabaseDriver;
 
-    
-// @SpringBootApplication
+@SpringBootApplication
+// these next two are redundant until we kill @SpringBootApplication
 @ComponentScan
+@Configuration
 public class AccessingDataPgApplication {
+
+    private static String dbUrl;
     
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
-
+        dbUrl = dotenv.get("DB_URL");
         // SpringApplication.run(AccessingDataPgApplication.class, args);
 
         AnnotationConfigApplicationContext appCtx = new AnnotationConfigApplicationContext(AccessingDataPgApplication.class);
@@ -26,4 +38,19 @@ public class AccessingDataPgApplication {
         MyTool t = appCtx.getBean(MyTool.class);
         t.run();
     }
+
+    @Bean
+		DataSource dataSource() {
+
+        String driverClassName = DatabaseDriver.fromJdbcUrl(dbUrl).getDriverClassName();
+        
+        HikariDataSource dataSource = DataSourceBuilder.create(ClassUtils.getDefaultClassLoader())
+            .type(HikariDataSource.class)
+            .driverClassName(driverClassName)
+            .url(dbUrl)
+            .build();
+				dataSource.setPoolName("default");
+        return dataSource;
+		}
+    
 }
