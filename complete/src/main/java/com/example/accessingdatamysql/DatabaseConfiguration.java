38fa1c82acf7355682480f2f4744a132210d9ead
Michael Rivera <soymrmike@gmail.com>
2023-06-05 08:45:33 -0700

    Removed some logging.

--- complete/src/main/java/com/example/accessingdatamysql/DatabaseConfiguration.java
+++ complete/src/main/java/com/example/accessingdatamysql/DatabaseConfiguration.java
@@ -22,15 +22,6 @@ public class DatabaseConfiguration {
     @Bean
     public TransactionManagerCustomizers
         platformTransactionManagerCustomizers(ObjectProvider<PlatformTransactionManagerCustomizer<?>> customizers) {
-
-        System.out.println("**************************************************");
-        System.out.println("Mike's platformTransactionManagerCustomizers bean");
-        Object something = customizers.getIfAvailable();
-        if(something != null) {
-            System.out.println("Got something");
-            System.out.println(something);
-        }
-        
         return new TransactionManagerCustomizers(customizers.orderedStream().toList());
     }
 
